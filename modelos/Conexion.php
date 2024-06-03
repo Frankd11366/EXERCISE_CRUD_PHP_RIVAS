4be6df962a43460eef023628422a40bbf9f125ba
Frankd11366 <capurivas@gmail.com>
2024-06-02 21:56:19 -0600

    Se mejoraron los modelos

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -1,23 +1,30 @@
 <?php
 
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+
+
 abstract class Conexion{
     protected static $conexion = null;
 
     protected static function connectar() : PDO{
         try {
+            
             self::$conexion = new PDO("informix:host=host.docker.internal; service=9088;database=tienda; server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix", "in4mix");
             self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+            echo "conexion exitosa";
         } catch (PDOException $e) {
             echo "No hay conexion a la BD";
             echo "<br>";
             echo $e->getMessage();
             self::$conexion = null;
-            return null;
             exit;
         }
 
         return self::$conexion;
+
     }
 
     // METODO PARA EJECUTAR SENTENCIAS SQL
