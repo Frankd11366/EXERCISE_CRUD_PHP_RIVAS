86bc86cb456f4ebc0467a79dc17e0cf834d6001d
Frankd11366 <capurivas@gmail.com>
2024-06-02 22:17:50 -0600

    modificar, buscar creado

--- vistas/cliente/buscar.php
+++ vistas/cliente/buscar.php
@@ -1,42 +1,31 @@
-<?php include_once '../templates/header.php'; ?>
+<?php 
 
-<h1 class="text-center">Formulario de clientes</h1>
+include_once '../../vistas/templates/header.php'; ?>
+
+<h1 class="text-center">FORMULARIO DE CLIENTES</h1>
 <div class="row justify-content-center">
-    <form action="/crud_2024/controladores/cliente/buscar.php" method="GET" class="border bg-light shadow rounded p-4 col-lg-6">
+    <form action="../../controladores/clientes/buscar.php" method="GET" class="border bg-light shadow rounded p-4 col-lg-6">
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nombre">Nombre del cliente</label>
+                <label for="cli_nombre">NOMBRE</label>
                 <input type="text" name="cli_nombre" id="cli_nombre" class="form-control" >
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_apellido">Apellido del cliente</label>
+                <label for="cli_apellido">APELLIDO</label>
                 <input type="text" name="cli_apellido" id="cli_apellido" class="form-control" >
             </div>
         </div>
-        <div class="row mb-3">
+        
+        <div class="row">
             <div class="col">
-                <label for="cli_nit">NIT del cliente</label>
-                <input type="number" name="cli_nit" id="cli_nit" step="1" class="form-control" >
-            </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col">
-                <label for="cli_telefono">Teléfono del cliente</label>
-                <input type="tel" name="cli_telefono" id="cli_telefono" step="1" class="form-control" >
-            </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col">
-                <button type="submit" class="btn btn-info w-100"><i class="bi bi-search me-2"></i>Buscar</button>
+                <button type="submit" class="btn btn-info w-100 bg-dark text-white"> BUSCAR</button>
             </div>
         </div>
     </form>
 </div>
 
-<?php include_once '../templates/footer.php'; ?>
-
-
+<?php include_once '../../vistas/templates/footer.php'; ?>
 
    
--- vistas/cliente/index.php
+++ vistas/cliente/index.php
@@ -1,46 +1,47 @@
-<?php include_once '../templates/header.php'; ?>
+<?php 
 
-<h1 class="text-center">Formulario de clientes</h1>
+include_once '../../vistas/templates/footer.php'; ?>
+
+<h1 class="text-center">FORMULARIO DE CLIENTES</h1>
 <div class="row justify-content-center">
-    <form action="/crud_2024/controladores/cliente/guardar.php" method="POST" class="border bg-light shadow rounded p-4 col-lg-6">
+    <form action="../../controladores/cliente/guardar.php" method="POST" class="border bg-light shadow rounded p-4 col-lg-6">
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nombre">Nombre del cliente</label>
+                <label for="cli_nombre">NOMBRE</label>
                 <input type="text" name="cli_nombre" id="cli_nombre" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_apellido">Apellido del cliente</label>
+                <label for="cli_apellido">APELLIDO</label>
                 <input type="text" name="cli_apellido" id="cli_apellido" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_nit">NIT del cliente</label>
-                <input type="number" name="cli_nit" id="cli_nit" step="1" class="form-control" required>
+                <label for="cli_nit">NIT</label>
+                <input type="number" name="cli_nit" id="cli_nit" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <label for="cli_telefono">Teléfono del cliente</label>
-                <input type="tel" name="cli_telefono" id="cli_telefono" step="1" class="form-control" required>
+                <label for="cli_telefono">TELEFONO</label>
+                <input type="number" name="cli_telefono" id="cli_telefono" class="form-control" required>
             </div>
         </div>
         <div class="row mb-3">
             <div class="col">
-                <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                <button type="submit" class="btn btn-secondary w-100">GUARDAR</button>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <a href="../../controladores/cliente/buscar.php" class="btn btn-info w-100">Buscar</a>
+                <a href="../../controladores/cliente/buscar.php" class="btn btn-secondary w-100">BUSCAR</a>
             </div>
         </div>
     </form>
 </div>
 
-<?php include_once '../templates/footer.php'; ?>
-
+<?php include_once '../../vistas/templates/footer.php'; ?>
 
    
