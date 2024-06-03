238d9efa43ed35e04a81a9aa6bef4f7466244e31
Frankd11366 <capurivas@gmail.com>
2024-06-02 23:20:04 -0600

    imagen actualizada

--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../../vistas/cliente/index.php"><img src="../../src/images/logo.png" alt="Logo" width="30" height="30">CLIENTES</a>
+    <a class="navbar-brand" href="../../vistas/cliente/index.php"><img src="../../src/images/apple.png" alt="Logo" width="30" height="30">CLIENTES</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       
     </button>
