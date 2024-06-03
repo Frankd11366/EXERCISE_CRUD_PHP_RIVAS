e512ec672e9d38034a7e7274eb8164f2526378a6
Frankd11366 <capurivas@gmail.com>
2024-06-02 22:26:26 -0600

    creacion de navbar

--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -1,39 +1,21 @@
 <nav class="navbar fixed-top navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/crud_2024/vistas/producto/index.php"><img src="/crud_2024/src/images/logo.png" alt="Logo" width="30" height="30">Tienda APP</a>
+    <a class="navbar-brand" href="../../vistas/cliente/index.php"><img src="../../src/images/logo.png" alt="Logo" width="30" height="30">CLIENTES</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house-fill me-2"></i>Inicio</a>
+          <a class="nav-link active" aria-current="page" href="../../vistas/cliente/index.php"><i class="bi bi-house-fill me-2"></i>INICIO</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-boxes me-2"></i>Ventas
+            <i class="bi bi-file-person me-2"></i>CLIENTES
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/crud_2024/vistas/ventas/index.php"><i class="bi bi-plus-circle me-2"></i>Ingreso de ventas</a></li>
-            <!-- <li><a class="dropdown-item" href="/crud_2024/vistas/producto/buscar.php"><i class="bi bi-search me-2"></i>Buscar</a></li> -->
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-boxes me-2"></i>Productos
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/crud_2024/vistas/producto/index.php"><i class="bi bi-plus-circle me-2"></i>Crear</a></li>
-            <li><a class="dropdown-item" href="/crud_2024/vistas/producto/buscar.php"><i class="bi bi-search me-2"></i>Buscar</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="bi bi-people me-2"></i>Clientes
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/crud_2024/vistas/cliente/index.php"><i class="bi bi-plus-circle me-2"></i>Crear</a></li>
-            <li><a class="dropdown-item" href="/crud_2024/vistas/cliente/buscar.php"><i class="bi bi-search me-2"></i>Buscar</a></li>
+            <li><a class="dropdown-item" href="../../vistas/cliente/index.php"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
+            <li><a class="dropdown-item" href="../cliente/buscar.php"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
           </ul>
         </li>
     </div>
